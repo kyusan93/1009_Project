304361aa8e1f24cb827d85ee39c12c3df3e4ac52
P2_GohThiamAik <71330661+kyusan93@users.noreply.github.com>
2022-03-23 04:31:51 +0000

    Test

--- .ccls-cache/@home@runner@1009Project/main.cpp
+++ .ccls-cache/@home@runner@1009Project/main.cpp
@@ -1,4 +1,19 @@
-#include <iostream>
+#include<iostream>
+#include<string>
+
+using namespace std;
+// declaration section
+class Student {
+  private:
+    int idNum;
+    string lastName;
+    double gradePointAverage;
+  public:
+    void displayStudentData();
+    void setIdNum(int);
+    void setLastName(string);
+    void setGradePointAverage(double);
+};
 
 int main() {
   std::cout << "Hello World!\n";
